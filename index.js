4171584ef47f26c50623b9d2ffea5b140ee2edef
GomoxK <yatozamora1999@gmail.com>
2020-10-11 11:06:18 -0600

    Instalacion y configuracion de dotenv y del archivo de variables de entorno (.env)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Importar modulos requeridos para el servidor
 const express = require("express");
 const exphbs = require("express-handlebars");
-const router = require("./routes/index");
+//const router = require("./routes/index");
 
 //Habilitar el archivo de variables de entorno
 require("dotenv").config({ path: ".env" });
@@ -10,6 +10,12 @@ require("dotenv").config({ path: ".env" });
 const app = express();
 
 //Implementar router
-app.use("/", router());
+//app.use("/", router());
+
+app.use("/", (req, res, next) => {
+    res.send("Bienvenido a Cashize");
+});
+
+
 
 app.listen(process.env.PORT);
